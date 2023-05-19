7106f4ee7936890c75e008c90677eff58ce0e63f
janisgee <janisgeegee@gmail.com>
2023-05-19 18:58:31 +0800

    added reset button and scoreboard

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,48 @@
 let boxes;
 let data;
 let turn;
+let circleScore = 0;
+let crossScore = 0;
+
+const overlay = document.querySelector('.overlay');
+const hidden = document.querySelector('.hidden');
+
+const gameResult = document.querySelector('.game-result');
+
+const btnRestart = document.querySelector('.btn-restart');
+const btnReset = document.querySelector('.btn-reset-score');
+
+const crossScoreDisplay = document.querySelector('.cross-score');
+const circleScoreDisplay = document.querySelector('.circle-score');
+
+const restart = function () {
+  console.log('restart');
+  boxes.forEach((box) => (box.innerHTML = ''));
+  initialize();
+};
+btnRestart.addEventListener('click', restart);
+
+const resetScore = function () {
+  circleScoreDisplay.innerHTML = circleScore = 0;
+  crossScoreDisplay.innerHTML = crossScore = 0;
+  restart();
+};
+btnReset.addEventListener('click', resetScore);
 
 const initialize = function () {
-  boxes = document.querySelectorAll('.box');
   data = [[], [], []];
   turn = 0;
   let x = 0;
   let y = 0;
   let i = 0;
 
+  boxes = document.querySelectorAll('.box');
+
   boxes.forEach((box) => {
     box.x = x;
     box.y = y;
     box.i = i++;
-    data[y][x] = box;
+    data[y][x] = 0;
     x++;
     if (x > 2) {
       x = 0;
@@ -23,52 +51,101 @@ const initialize = function () {
     box.addEventListener('click', clickedBox);
   });
 };
+const PLAYER_1 = 1;
+const PLAYER_2 = -1;
+const NO_PLAYER = 0;
+const BOARD_SIZE = 3;
 
-const getScore = function (board) {
-  let win = 0;
-  for (let row = 0; row < board.length; row++) {
-    let score = 0;
-    for (let column = 0; column < board.length; column++) {
-      score += board[row][column];
+const getWinningPlayer = function (board) {
+  // 1) Check rows
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    let rowScore = 0;
+    for (let column = 0; column < BOARD_SIZE; column++) {
+      let box = board[row][column];
+      rowScore += box;
     }
-    if (score == 3) win = 1;
-    if (score == -3) win = -1;
+    if (rowScore === BOARD_SIZE * PLAYER_1) return PLAYER_1;
+    if (rowScore === BOARD_SIZE * PLAYER_2) return PLAYER_2;
   }
-  for (let column = 0; column < board.length; column++) {
-    let score = 0;
-    for (let row = 0; row < board.length; row++) {
-      score += board[row][column];
+
+  // 2) Check columns
+  for (let column = 0; column < BOARD_SIZE; column++) {
+    let columnScore = 0;
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      let box = board[row][column];
+      columnScore += box;
     }
-    if (score == 3) win = 1;
-    if (score == -3) win = -1;
+    if (columnScore === BOARD_SIZE * PLAYER_1) return PLAYER_1;
+    if (columnScore === BOARD_SIZE * PLAYER_2) return PLAYER_2;
   }
 
-  let score = 0;
-  for (let rowAndColumn = 0; rowAndColumn < 3; rowAndColumn++) {
-    score += board[rowAndColumn][rowAndColumn];
+  // 3) Check diagonal
+  let diagonalDownScore = 0;
+  for (let rowAndColumn = 0; rowAndColumn < BOARD_SIZE; rowAndColumn++) {
+    diagonalDownScore += board[rowAndColumn][rowAndColumn];
   }
-  if (score == 3) win = 1;
-  if (score == -3) win = -1;
+  if (diagonalDownScore === BOARD_SIZE * PLAYER_1) return PLAYER_1;
+  if (diagonalDownScore === BOARD_SIZE * PLAYER_2) return PLAYER_2;
 
-  score = 0;
-  for (let ii = 0; ii < 3; ii++) {
-    let row = 2 - ii;
-    let column = 0 + ii;
-    score += board[row][column];
+  let diagonalUpScore = 0;
+  for (let offset = 0; offset < BOARD_SIZE; offset++) {
+    let row = offset;
+    let column = 2 - offset;
+    diagonalUpScore += board[row][column];
   }
-  if (score == 3) win = 1;
-  if (score == -3) win = -1;
+  if (diagonalUpScore === BOARD_SIZE * PLAYER_1) return PLAYER_1;
+  if (diagonalUpScore === BOARD_SIZE * PLAYER_2) return PLAYER_2;
 
-  return win;
+  return NO_PLAYER;
 };
 
 const isBoardFull = function (board) {
-  for (let y = 0; y < board.length; y++) {
-    for (let x = 0; x < board.length; x++) {
-      if (board[y][x] === 0) {
-        return false;
-      }
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    for (let column = 0; column < BOARD_SIZE; column++) {
+      if (board[row][column] === 0) return false;
     }
+  }
+  return true;
+};
+
+const closeOverlay = function () {
+  let events = ['click', 'keydown'];
+  events.forEach((ev) =>
+    overlay.addEventListener(ev, function () {
+      overlay.classList.add('hidden');
+    }),
+  );
+};
+
+const result = function (win) {
+  if (win === PLAYER_2) {
+    boxes.forEach((box) => {
+      box.removeEventListener('click', clickedBox);
+    });
+    setTimeout(() => {
+      overlay.classList.toggle('hidden');
+      closeOverlay();
+      gameResult.innerHTML = 'Cross ❌ win !';
+      crossScoreDisplay.innerHTML = crossScore += 1;
+    }, 500);
+  }
+  if (win === PLAYER_1) {
+    boxes.forEach((box) => {
+      box.removeEventListener('click', clickedBox);
+    });
+    setTimeout(() => {
+      overlay.classList.toggle('hidden');
+      closeOverlay();
+      gameResult.innerHTML = 'Circle ⭕ win !';
+      circleScoreDisplay.innerHTML = circleScore += 1;
+    }, 500);
+  }
+  if (win === NO_PLAYER && isBoardFull(data)) {
+    setTimeout(() => {
+      overlay.classList.toggle('hidden');
+      closeOverlay();
+      gameResult.innerHTML = '🛎️ Draw game !';
+    }, 500);
   }
 };
 
@@ -77,22 +154,17 @@ const clickedBox = function (event) {
   if (box.innerHTML === '') {
     if (turn % 2 === 0) {
       box.innerHTML = "<i class='fa-regular fa-circle fa-2xs tic-circle'></i>";
-      data[box.y][box.x] = 1;
+      data[box.y][box.x] = PLAYER_1;
     } else {
       box.innerHTML = "<i class='fa-solid fa-xmark fa-sm tic-cross'></i>";
-      data[box.y][box.x] = -1;
+      data[box.y][box.x] = PLAYER_2;
     }
     turn++;
   }
 
-  let win = getScore(data);
+  let win = getWinningPlayer(data);
   console.log(win);
-
-  setTimeout(() => {
-    if (win === -1) alert('Cross win');
-    if (win === 1) alert('Circle win');
-    if (win === 0 && isBoardFull(data)) alert('Draw game!');
-  }, 100);
+  result(win);
 };
 
 initialize();
